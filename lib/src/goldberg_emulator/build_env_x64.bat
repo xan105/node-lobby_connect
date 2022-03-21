23155524c43aa289c6e0c4728c56565074f7b626
Anthony Beaumont <dev.xan105@outlook.be>
2022-03-21 13:57:31 +0700

    vs2022

--- lib/src/goldberg_emulator/build_env_x64.bat
+++ lib/src/goldberg_emulator/build_env_x64.bat
@@ -4,12 +4,17 @@ rem Put in the base path in which Visual Studio is installed, default would be C
 
 set VS_Base_Path=C:\Program Files (x86)
 
+if exist "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" goto vs2022
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat" goto vs2019
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat" goto vs2017
 if exist "%VS_Base_Path%\Microsoft Visual Studio 14.0\VC\bin\amd64\vcvars64.bat" goto vs14
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto vs2019_bt
 if exist "%VS_Base_Path%\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat" goto vs2017_bt
 if exist ".\sdk_standalone\set_vars64.bat" goto gitlabci
+
+:vs2022
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+goto batend
 
 :vs2019
 call "%VS_Base_Path%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
